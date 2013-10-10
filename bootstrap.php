5b7a51d8ed5470f721bb508db5d0e8975ec59ffd
Ryan Van Etten <ryanve@gmail.com>
2013-10-10 05:34:37 -0400

    find() can be used to walk()

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,6 @@
 <?php
-trait_exists('\\slash\\traits\\Mixin') or require './slash/traits/Mixin.php';
-class_exists('\\slash\\Slash') or require './slash/Slash.php';
-class_exists('\\slash\\Path') or require './slash/Path.php';
-class_exists('\\slash\\File') or require './slash/File.php';
-class_exists('\\slash\\Uri') or require './slash/Uri.php';
+trait_exists('\\slash\\traits\\Mixin') or require 'slash/traits/Mixin.php';
+class_exists('\\slash\\Slash') or require 'slash/Slash.php';
+class_exists('\\slash\\Path') or require 'slash/Path.php';
+class_exists('\\slash\\File') or require 'slash/File.php';
+class_exists('\\slash\\Uri') or require 'slash/Uri.php';
