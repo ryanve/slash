075b133fc5715800dce074e0a0c3dfb9912e0e5f
Ryan Van Etten <ryanve@gmail.com>
2013-10-10 03:23:44 -0400

    Add bootstrap file.

---
+++ slash.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @link https://github.com/ryanve/slash
+ * @license MIT
+ */
+// bootstrap
+trait_exists('\\slash\\traits\\Mixin') or require './slash/traits/Mixin.php';
+class_exists('\\slash\\Slash') or require './slash/Slash.php';
+class_exists('\\slash\\Path') or require './slash/Path.php';
+class_exists('\\slash\\File') or require './slash/File.php';
+class_exists('\\slash\\Uri') or require './slash/Uri.php';
